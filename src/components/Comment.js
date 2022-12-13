0a045e15b22c731c9c85db540857054c6bf071cf
hamid <hd.karimpoor@gmail.com>
2022-12-13 15:58:20 -0500

    add comment sections

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,16 @@
 import styles from '../styles/Comment.module.css';
 
-export default function Comment({ cmtUUID }) {
+export default function Comment({ uuid, text }) {
     return (
 
-        <div id={cmtUUID} className={styles.container}>
+        <div id={uuid} className={styles.container}>
             <div className={styles.avatar_section}>
                 <img src="abc" alt="abc" />
             </div>
             <div className={styles.content_section}>
-                <div></div>
+                <div className={styles.info_section}></div>
+                <p className={styles.text_section}>{text}</p>
+                <div className={styles.actions_section}></div>
             </div>
         </div>     
         
--- src/pages/react-redtube-v1.js
+++ src/pages/react-redtube-v1.js
@@ -5,16 +5,16 @@ export default function ReactRedtubeV1() {
 
     const comments= [
         {uuid: '1', text:'abc'},
-        {uuid: '1', text:'abc'},
-        {uuid: '1', text:'abc'},
-        {uuid: '1', text:'abc'},
-        {uuid: '1', text:'abc'},
-        {uuid: '1', text:'abc'},
-        {uuid: '1', text:'abc'}
+        {uuid: '2', text:'abc'},
+        {uuid: '3', text:'abc'},
+        {uuid: '4', text:'abc'},
+        {uuid: '5', text:'abc'},
+        {uuid: '6', text:'abc'},
+        {uuid: '7', text:'abc'}
     ];
     return (<>
-        {comments.map((comments, key)=>{
-            return (<Comment cmtUUID={key} ></Comment>);
+        {comments.map((comment, key)=>{
+            return (<Comment comment={comment} key={comment.uuid}></Comment>); 
         })}
     </>);
 }
